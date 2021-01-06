e3d6e80d537cb2d5906df9a913a28063380ebc7e
qhahd78 <umuj0708@likelion.org>
2021-01-07 08:52:43 +0900

    react(2)

--- boiler-plate/client/src/App.js
+++ boiler-plate/client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          안뇽
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+            
+          <Route exact path="/login" component={LoginPage} />
+          
+          <Route exact path="/register" component={RegisterPage} />
+  
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
